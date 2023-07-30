e3904ddcdf4e70cb89d102e6277221e1a44a9909
janezb10 <janezb10@gmail.com>
2023-07-30 11:54:22 +0200

    feat: Add authentication utilities

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const jwt = require("jsonwebtoken"); // jwt
-
 const app = express();
-const secretKey = "akdkdkd"; // jwt
+const { generateJWTToken, verifyJWTToken } = require("./utils/authUtils");
+
+const routes = require("./routes/index"); // routes
 
 app.use(express.json());
+app.use(express.static("./static"));
 
 const users = [
   { id: 1, username: "user1", password: "pass1" },
   { id: 2, username: "user2", password: "pass2" },
 ];
-
-// Function for generating JWT token
-function generateJWTToken(payload) {
-  return jwt.sign(payload, secretKey, { expiresIn: "1h" });
-}
 
 // Endpoint to login
 app.post("/login", (req, res) => {
@@ -31,36 +27,11 @@ app.post("/login", (req, res) => {
   const token = generateJWTToken({ userId: user.id, username: user.username });
   res.send({ token });
 });
-// Middleware to check JWT token
-function verifyJWTToken(req, res, next) {
-  const token = req.header("Authorization")?.replace("Bearer ", ""); // We get the JWT token from the authorization header
 
-  if (!token) {
-    return res.status(401).json({ message: "Missing authorization token" });
-  }
-
-  //   We check and decode JWT token
-  jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res
-        .status(403)
-        .json({ message: "Neveljaven avtotizacijski žeton." });
-    }
-
-    req.user = decoded; // Dodamo dekodirane podatke iz žetona
-    next();
-  });
-}
-
-// Zaščiten endpoint - samo avtenticirani uporabniki lahko dostopajo do tega
-app.get("/protected", verifyJWTToken, (req, res) => {
-  const user = req.user;
-  res.json({ message: `Ẁelcome, ${user.username}! Ta stran je zaščitena.` });
-});
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+// Authenticate Users
+app.use(verifyJWTToken);
+// Only authenticated users have access
+app.use("/", routes);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ backend/utils/authUtils.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = "akdkdkd";
+
+// Function for generating JWT token
+function generateJWTToken(payload) {
+  return jwt.sign(payload, secretKey, { expiresIn: "1h" });
+}
+
+// verify JWT token
+function verifyJWTToken(req, res, next) {
+  const token = req.header("Authorization")?.replace("Bearer ", ""); // We get the JWT token from the authorization header
+
+  if (!token) {
+    return res.status(401).json({ message: "Missing authorization token" });
+  }
+  //   We check and decode JWT token
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .json({ message: "Neveljaven avtotizacijski žeton." });
+    }
+    req.user = decoded; // Dodamo dekodirane podatke iz žetona
+    next();
+  });
+}
+
+module.exports = {
+  generateJWTToken,
+  verifyJWTToken,
+};
