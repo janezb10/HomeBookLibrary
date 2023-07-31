12364c2eba1d7ff85dd0b70147e874dd08b072b5
janezb10 <janezb10@gmail.com>
2023-07-31 17:18:53 +0200

    login form validation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,7 +14,7 @@ const schema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
-    .min(6, { message: "Password must be at least 6 characters long" }),
+    .min(6, { message: "Password must be at least 6 characters long." }),
 });
 
 type LoginFormValues = z.infer<typeof schema>;
