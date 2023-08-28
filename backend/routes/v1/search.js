f901a304c53dab0ce8f3dbc0fa80f94c5e21e78e
janezb10 <janezb10@gmail.com>
2023-08-28 12:25:51 +0200

    filters work on backend

--- backend/routes/v1/search.js
+++ backend/routes/v1/search.js
@@ -4,17 +4,73 @@ const db = require("../../config/SQLConfig");
 
 router.get("/:keyword", async (req, res, next) => {
   try {
+    const positions = req.query.positions;
+    const authors = req.query.authors;
+    const languages = req.query.languages;
+    const collections = req.query.collections;
+    const fields = req.query.fields;
+
+    const positionValues = positions ? positions.split(",").map(Number) : [];
+    const authorsValues = authors ? authors.split(",").map(Number) : [];
+    const languagesValues = languages ? languages.split(",").map(Number) : [];
+    const collectionsValues = collections
+      ? collections.split(",").map(Number)
+      : [];
+    const fieldsValues = fields ? fields.split(",").map(Number) : [];
+
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 25;
-
     const offset = (page - 1) * limit;
 
     const countSql = `
     SELECT COUNT(*) AS totalCount
     FROM books
-    WHERE books.title LIKE ?;
-    `;
-    const [countRows] = await db.execute(countSql, [`%${req.params.keyword}%`]);
+    WHERE books.title LIKE ?
+        ${
+          positions
+            ? ` AND books.id_position IN (${positionValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }      
+        ${
+          authors
+            ? ` AND books.id_author IN (${authorsValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }        
+        ${
+          languages
+            ? ` AND books.id_language IN (${languagesValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }        
+        ${
+          collections
+            ? ` AND books.id_collection IN (${collectionsValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }        
+        ${
+          fields
+            ? ` AND books.id_field IN (${fieldsValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }
+    ;`;
+
+    const [countRows] = await db.execute(countSql, [
+      `%${req.params.keyword}%`,
+      ...positionValues,
+      ...authorsValues,
+      ...languagesValues,
+      ...collectionsValues,
+      ...fieldsValues,
+    ]);
     const totalCount = countRows[0].totalCount;
     const numberOfPages = Math.ceil(totalCount / limit);
 
@@ -29,17 +85,57 @@ router.get("/:keyword", async (req, res, next) => {
          LEFT JOIN collections ON books.id_collection = collections.id_collection
         WHERE books.title
         LIKE ?
+        ${
+          positions
+            ? ` AND books.id_position IN (${positionValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }      
+        ${
+          authors
+            ? ` AND books.id_author IN (${authorsValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }        
+        ${
+          languages
+            ? ` AND books.id_language IN (${languagesValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }        
+        ${
+          collections
+            ? ` AND books.id_collection IN (${collectionsValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }        
+        ${
+          fields
+            ? ` AND books.id_field IN (${fieldsValues
+                .map((e) => "?")
+                .join(",")}) `
+            : ""
+        }
         LIMIT ? , ? ;`;
 
     const [rows] = await db.execute(sql, [
       `%${req.params.keyword}%`,
+      ...positionValues,
+      ...authorsValues,
+      ...languagesValues,
+      ...collectionsValues,
+      ...fieldsValues,
       `${offset}`,
       `${limit}`,
     ]);
 
-    // if (rows.length === 0) throw new Error("Nothing was found");
     res.send({
       books: rows,
+      // a: sql,
       numberOfPages: numberOfPages,
       currentPage: page,
     });
