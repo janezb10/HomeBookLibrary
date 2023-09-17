4f68f1feed76ba5a83d9684874c65d9b3eeb774c
janezb10 <janezb10@gmail.com>
2023-09-17 11:51:57 +0200

    baseURL: location.hostname,

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios, { CanceledError, AxiosError } from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: location.hostname,
 });
 
 export { CanceledError, AxiosError };
